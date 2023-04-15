e7158b8170264aab63d81e1678cbd266511bd8d8
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-04-15 17:56:02 -0300

    Cleans index.php

--- from-static-to-dinamic/public/index.php
+++ from-static-to-dinamic/public/index.php
@@ -1,22 +1,12 @@
 <?php
     require __DIR__."/../src/bootstrap.php";
-    use PAW\Core\Router;
     use PAW\Core\Exceptions\RouteNotFoundException;
     
     $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-    
-    $router = new Router;
-    $router -> loadRoutes("/", "PageController@index");
-    $router -> loadRoutes("/about", "PageController@about");
-    $router -> loadRoutes("/services", "PageController@services");
-    $router -> loadRoutes("/contact", "PageController@contact");
-    $router -> loadRoutes("/notFound", "ErrorController@notFound");
-    $router -> loadRoutes("/internalError", "ErrorController@internalError");
-
+    $logger -> info("Request to:" . $path);
     try {
         $router -> direct($path);
         $logger -> info("Successful request: 200", ["INFO" => $path]);
-        
     } catch (RouteNotFoundException $exception) {
         $router -> direct("/notFound");
         $logger -> info("Status Code: 404 - Route Not Found", ["INFO" => $path]);
--- from-static-to-dinamic/src/bootstrap.php
+++ from-static-to-dinamic/src/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
     require __DIR__."/../vendor/autoload.php";
-
+    
+    use PAW\Core\Router;
     use Monolog\Logger;
     use Monolog\Handler\StreamHandler;
 
@@ -10,5 +11,13 @@
     $whoops = new \Whoops\Run; // Crea una instancia de la clase "Run" de la libreria "Whoops".
     $whoops -> pushHandler(new \Whoops\Handler\PrettyPageHandler); // Crea una instancia de la clase "PrettyHandler" de la libreria "Whoops\Handler".
     $whoops -> register();
+
+    $router = new Router;
+    $router -> loadRoutes("/", "PageController@index");
+    $router -> loadRoutes("/about", "PageController@about");
+    $router -> loadRoutes("/services", "PageController@services");
+    $router -> loadRoutes("/contact", "PageController@contact");
+    $router -> loadRoutes("/notFound", "ErrorController@notFound");
+    $router -> loadRoutes("/internalError", "ErrorController@internalError");
 
 ?>
